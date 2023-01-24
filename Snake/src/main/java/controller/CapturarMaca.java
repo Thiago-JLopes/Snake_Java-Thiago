070b056e8d38328e0fed317e980b9b6e8edb183a
Thiago Lopes <tiagojosecnn@outlook.com>
2023-01-24 17:48:43 -0300

    Cria classe para verificar se a maça foi capturada.

---
+++ Snake/src/main/java/controller/CapturarMaca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.DrawPanel;
+
+/**
+ *
+ * @author thiago
+ */
+public class CapturarMaca {
+    public static void capturar(int pX[], int pY[]) {
+        if(pX[0] == DrawPanel.getAppleX() && pY[0] == DrawPanel.getAppleY()) {
+            DrawPanel.setSnakeSize(DrawPanel.getSnakeSize()+1);
+            DrawPanel.createApple();
+        }
+    }
+}
