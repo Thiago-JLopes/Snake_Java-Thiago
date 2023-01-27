61f30f00f8061167fd5f267ca42c39c18d62157b
Thiago Lopes <tiagojosecnn@outlook.com>
2023-01-26 21:54:39 -0300

    Adiciona impressão da posção do evento, testa condição de fim do jogo.

--- Snake/src/main/java/controller/validarPosicoes.java
+++ Snake/src/main/java/controller/validarPosicoes.java
@@ -15,16 +15,19 @@ public class validarPosicoes {
     public static void validarPosicoes(int pX[], int pY[]) {
         for (int i = DrawPanel.getSnakeSize(); i > 0; i--) {
             if (pX[0] == pX[i] && pY[0] == pY[i]) {
+                System.out.println("Cobra de tocou!");
                 DrawPanel.setExecutando(false);
                 break;
             }
         }
 
-        if (pX[0] < 0 || pX[0] > DrawPanel.getLARGURA_TELA()) {
+        if ((pX[0]) < 0 || pX[0]  > DrawPanel.getLARGURA_TELA()) {
+            System.out.println("excedeu largura");
             DrawPanel.setExecutando(false);
         }
 
         if (pY[0] < 0 || pY[0] > DrawPanel.getALTURA_TELA()) {
+            System.out.println("Excedeu altura");
             DrawPanel.setExecutando(false);
         }
         
