570c2e1ec5323b2b368e5dad020a2f0a767fa938
Thiago Lopes <tiagojosecnn@outlook.com>
2023-01-27 20:33:03 -0300

    Adiciona dependencia GSON.

--- Snake/src/main/java/model/jogador.java
+++ Snake/src/main/java/model/jogador.java
@@ -9,5 +9,36 @@ package model;
  * @author thiago
  */
 public class jogador {
+    private final String nome;
+    private final String idade;
+    private final String email;
+    private int record;
+
+    public jogador(String nome, String idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public void setRecord(int record) {
+        this.record = record;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRecord() {
+        return record;
+    }
+    
     
 }
