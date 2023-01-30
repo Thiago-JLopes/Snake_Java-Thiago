c2a01c8b0e94731849806c61107ac1bcc3e9246e
Thiago Lopes <tiagojosecnn@outlook.com>
2023-01-30 18:54:05 -0300

    Adiciona imagem de fundo, cria lista de seleção de nível e botão play.

--- Snake/src/main/java/view/telaLogin.java
+++ Snake/src/main/java/view/telaLogin.java
@@ -5,22 +5,52 @@
 package view;
 
 import java.awt.*;
-import javax.swing. *;
+import javax.swing.*;
+
 /**
  *
  * @author thiago
  */
 public class telaLogin extends JPanel {
 
+    private JComboBox<String> levelsComboBox;
+    private final Image backGroundImage;
+    private static final String FONTE = "Ink Free";
+
     public telaLogin() {
         this.setPreferredSize(new Dimension(400, 200));
         this.setLayout(new BorderLayout());
         this.setFocusable(true);
-        desenharTelaLogin();
+        this.backGroundImage = new ImageIcon("imagem/fundo1.png").getImage();
+        LevelSelector();
     }
 
-    public void desenharTelaLogin() {
-        
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backGroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    private void LevelSelector() {
+        setLayout(null);
+        JLabel levelsLabel = new JLabel("SELECIONE O NÍVEL:");
+        levelsLabel.setBounds(120, 57, 200, 25);
+        levelsLabel.setForeground(new Color(3, 187, 133));
+        levelsLabel.setFont(new Font(FONTE, Font.BOLD, 14));
+        add(levelsLabel);
+
+        levelsComboBox = new JComboBox<>();
+        levelsComboBox.addItem("Fácil");
+        levelsComboBox.addItem("Médio");
+        levelsComboBox.addItem("Difícil");
+        levelsComboBox.setBounds(141, 77, 100, 25);
+
+        add(levelsComboBox, BorderLayout.WEST);
+
+        JButton start = new JButton("PLAY");
+        start.setBounds(290, 145, 100, 25);
+        start.setBackground(new Color(255, 105, 97));
+        add(start);
     }
 
 }
